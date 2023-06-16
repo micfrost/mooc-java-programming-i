2a8bc282827f1d36ccc2c602fa39d2f2c3dc9646
MicFro <116974794+MicFrost@users.noreply.github.com>
2023-06-16 22:02:51 +0200

    Squared
    Squared

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -5,6 +5,7 @@ public class Squared {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int givenNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println(givenNumber * givenNumber);
     }
 }

6a1029b80144d46d89b8c658c396ef3f40de9068
MicFro <116974794+MicFrost@users.noreply.github.com>
2023-06-17 20:38:28 +0200

    SqRooofSum

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -7,5 +7,10 @@ public class SquareRootOfSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int firstNumber = Integer.valueOf(scanner.nextLine());
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        double squareRoot = Math.sqrt(firstNumber + secondNumber);
+        if (firstNumber + secondNumber >= 0) {
+            System.out.println(squareRoot);
+        }
     }
 }

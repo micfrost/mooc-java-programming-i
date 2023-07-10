954a4125b956835e41ff53517bfe5c96a715ac20
MicFro <116974794+MicFrost@users.noreply.github.com>
2023-07-10 23:38:28 +0200

    OnlyPositives

--- part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -6,5 +6,18 @@ public class OnlyPositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Give a number:");
+            int givenNumber = Integer.valueOf(scanner.nextLine());
+            if (givenNumber < 0) {
+                System.out.println("Unsuitable number");
+                continue;
+            }
+            if (givenNumber == 0) {
+                break;
+            }
+            System.out.println(givenNumber * givenNumber);
+        }
     }
 }
+

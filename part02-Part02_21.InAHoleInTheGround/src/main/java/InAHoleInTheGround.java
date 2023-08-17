15d1a8d61255c371852195a0472f0cd5f66270e1
MicFro <michalfrost71@gmail.com>
2023-08-17 21:23:20 +0200

    init commit

--- part02-Part02_21.InAHoleInTheGround/src/main/java/InAHoleInTheGround.java
+++ part02-Part02_21.InAHoleInTheGround/src/main/java/InAHoleInTheGround.java
@@ -3,8 +3,10 @@ public class InAHoleInTheGround {
     public static void main(String[] args) {
         printText();
     }
-    
+
+
     public static void printText() {
+        System.out.println("hh");
         // Write some code in here
     }
 }

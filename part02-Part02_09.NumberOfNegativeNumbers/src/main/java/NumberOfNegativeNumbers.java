7741f2bd35021323bdbd2bdb63a8b734c677571d
MicFro <michalfrost71@gmail.com>
2023-08-17 22:37:33 +0200

    init commit

--- part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -1,10 +1,26 @@
-
 import java.util.Scanner;
 
 public class NumberOfNegativeNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
+
+        while (true) {
+            System.out.println("Give a number: ");
+            int givenNumber = Integer.valueOf(scanner.nextLine());
+
+            if (givenNumber == 0) {
+                break;
+            }
+            if (givenNumber < 0) {
+                count++;
+            }
+
+        }
+        System.out.println("Number of negative numbers: " + count);
+
 
     }
 }
--- part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -6,5 +6,19 @@ public class SumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum=0;
+        while (true) {
+            System.out.println("Give a number: ");
+            int givenNumber = Integer.valueOf(scanner.nextLine());
+
+            if (givenNumber == 0) {
+                break;
+            }
+            sum = sum + givenNumber;
+
+        }
+
+        System.out.println("Sum of the numbers: " + sum);
+
     }
 }

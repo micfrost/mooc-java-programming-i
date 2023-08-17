600e0b9bffc48d24961d044de82bed5428d5ad22
MicFro <michalfrost71@gmail.com>
2023-08-17 22:07:29 +0200

    init commit

--- part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -5,7 +5,7 @@ public class OnlyPositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        while (true && true) {
+        while (true) {
             System.out.println("Give a number:");
             int givenNumber = Integer.valueOf(scanner.nextLine());
             if (givenNumber < 0) {
--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,22 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("hehe");
+
+        int count = 0;
+
+        int givenNumber;
+
+        do {
+            System.out.println("Give a number: ");
+            givenNumber = scanner.nextInt();
+            count++;
+            if (givenNumber == 0) {
+                count--;
+            }
+        }
+        while (givenNumber != 0);
+
+        System.out.println("Number of numbers: " + count);
+
     }
 }

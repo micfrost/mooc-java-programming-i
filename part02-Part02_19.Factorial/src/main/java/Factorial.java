c5805822123b1fc5c12988f0ee30bb340caf1523
MicFro <116974794+micfrost@users.noreply.github.com>
2023-08-20 21:49:40 +0200

    Update Factorial.java
    update

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -3,8 +3,19 @@ import java.util.Scanner;
 
 public class Factorial {
 
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Give a number? ");
+        int givenNumber = scanner.nextInt();
+
+
+        int sum = 1;
+
+        for (int i = 1; i <= givenNumber; i++) {
+            sum = sum * i;
+        }
+        System.out.printf("Factorial: " + sum);
     }
 }

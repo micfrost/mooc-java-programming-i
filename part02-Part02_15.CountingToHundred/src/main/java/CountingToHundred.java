498548a954ea09646e6b6179eea5f6d4228a3549
MicFro <116974794+micfrost@users.noreply.github.com>
2023-08-20 21:26:40 +0200

    Update CountingToHundred.java
    update

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -6,5 +6,11 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int givenInput = scanner.nextInt();
+
+        for (int i=givenInput;i<=100;i++) {
+            System.out.println(i);
+        }
+
     }
 }

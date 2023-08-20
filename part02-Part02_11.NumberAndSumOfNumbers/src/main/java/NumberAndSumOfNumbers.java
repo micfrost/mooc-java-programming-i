105ccf374aaf50bd00f4d0519a5cbe997a5403a0
MicFro <116974794+micfrost@users.noreply.github.com>
2023-08-20 15:14:36 +0200

    Update NumberAndSumOfNumbers.java
    update

--- part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -5,6 +5,25 @@ public class NumberAndSumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum=0;
+        int inputTimes = 0;
+
+        while (true) {
+            System.out.println("Give a number: ");
+            int givenNumber = Integer.valueOf(scanner.nextLine());
+
+            if (givenNumber == 0) {
+                break;
+            }
+
+            inputTimes++;
+            sum = sum + givenNumber;
+
+
+        }
+        System.out.println("Number of numbers: " + inputTimes);
+
+        System.out.println("Sum of the numbers: " + sum);
 
     }
 }

6d4c4c062e4386be80c27ae3f7e9b4b971379968
MicFro <116974794+micfrost@users.noreply.github.com>
2023-08-20 21:35:46 +0200

    Update FromWhereToWhere.java
    update

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -6,10 +6,13 @@ public class FromWhereToWhere {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Where to?");
-        int givenInput = scanner.nextInt();
+        System.out.print("Where to? ");
+        int end = scanner.nextInt();
 
-        for (int i = 1; i <= givenInput; i++) {
+        System.out.print("Where from? ");
+        int start = scanner.nextInt();
+
+        for (int i = start; i <= end; i++) {
             System.out.println(i);
         }
 

5eb9085ecff9b9b94c7e6ea09462ef5bc6e2c17b
MicFro <116974794+micfrost@users.noreply.github.com>
2023-08-20 21:07:35 +0200

    Update AverageOfPositiveNumbers.java
    update

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,32 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double sum = 0;
+        double inputTimes = 0;
+
+        double ave;
+
+        while (true) {
+            System.out.println("Give a number: ");
+            double givenNumber = Double.valueOf(scanner.nextLine());
+
+            if (givenNumber == 0) {
+                break;
+            }
+
+            if (givenNumber>0) {
+                inputTimes++;
+                sum = sum + givenNumber;
+            }
+
+
+
+        }
+        if (inputTimes==0) {
+            System.out.printf("Cannot calculate the average");
+        } else {
+            ave = sum / inputTimes * 1.0;
+            System.out.println("Average of the numbers: " + ave);
+        }
     }
 }

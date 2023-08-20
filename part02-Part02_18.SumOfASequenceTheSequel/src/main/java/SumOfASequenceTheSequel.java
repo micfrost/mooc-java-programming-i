6aba5480442a1b3e0e6af96eb0369b5d188f5d42
MicFro <116974794+micfrost@users.noreply.github.com>
2023-08-20 21:45:45 +0200

    Update SumOfASequenceTheSequel.java
    update

--- part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -3,8 +3,22 @@ import java.util.Scanner;
 
 public class SumOfASequenceTheSequel {
 
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("First number? ");
+        int first = scanner.nextInt();
+
+        System.out.print("Last number? ");
+        int last = scanner.nextInt();
+
+        int sum = 0;
+
+
+        for (int i = first; i <= last; i++) {
+            sum = sum + i;
+        }
+        System.out.printf("The sum is " + sum);
     }
 }

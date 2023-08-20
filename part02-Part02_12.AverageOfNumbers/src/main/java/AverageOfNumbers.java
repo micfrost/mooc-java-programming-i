b0ecd8a6d75ca5ae3f79dcfcfd437ad7f913bf20
MicFro <116974794+micfrost@users.noreply.github.com>
2023-08-20 20:58:29 +0200

    Update AverageOfNumbers.java
    update

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,5 +6,26 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double sum = 0;
+        double inputTimes = 0;
+
+        double ave;
+
+        while (true) {
+            System.out.println("Give a number: ");
+            double givenNumber = Double.valueOf(scanner.nextLine());
+
+            if (givenNumber == 0) {
+                break;
+            }
+
+            inputTimes++;
+            sum = sum + givenNumber;
+
+
+        }
+        ave = sum / inputTimes * 1.0;
+        System.out.println("Average of the numbers: " + ave);
+
     }
 }

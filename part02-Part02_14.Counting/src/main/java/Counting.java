38d814f1ffb4cc9371263a26ea7554dd651bfd04
MicFro <116974794+micfrost@users.noreply.github.com>
2023-08-20 21:23:48 +0200

    Update Counting.java
    update

--- part02-Part02_14.Counting/src/main/java/Counting.java
+++ part02-Part02_14.Counting/src/main/java/Counting.java
@@ -6,5 +6,11 @@ public class Counting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int givenInput = scanner.nextInt();
+
+        for (int i=0;i<=givenInput;i++) {
+            System.out.println(i);
+        }
+
     }
 }

3edc33375758083106b58d9e3a50c5868365d53c
MicFro <michalfrost71@gmail.com>
2023-11-03 20:36:44 +0100

    init commit

--- part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -3,6 +3,18 @@
 public class FromOneToParameter {
 
     public static void main(String[] args) {
+        printUntilNumber(5);
+
+    }
+
+//   defined method is above or under the main method but in this class.
+    public static void  printUntilNumber(int number) {
+        int i = 1;
+
+        while(i<=number) {
+            System.out.println(i);
+            i++;
+        }
 
     }
 
